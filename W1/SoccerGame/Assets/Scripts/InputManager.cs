ae928fe3fdecc30b02fab1852a35c63a86d99de1
zeeodd <evergreenskier@gmail.com>
2020-01-28 13:43:35 -0500

    Added Basics
    1. Added ball, players, goals, background 2. Input control for moving the ball 3. Physics interactions with player and ball 4. Boundary walls for the ball, although not for the player yet

---
+++ W1/SoccerGame/Assets/Scripts/InputManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    //-- Public vars --//
+    public float speed; // Controls player speed
+
+    //-- Private vars --//
+    private Rigidbody2D rb; // 2D rigidbody to grab
+    private Vector2 direction;
+    private Vector3 mousePosition;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            rb.velocity = Vector2.zero;
+        }
+        else
+        {
+            mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            direction = (mousePosition - transform.position).normalized;
+            rb.velocity = new Vector2(direction.x * speed, direction.y * speed);
+        }
+    }
+}
