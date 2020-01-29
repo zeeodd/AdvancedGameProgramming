c0dec8dfd985efc837ffd609d5308961b791efc6
zeeodd <evergreenskier@gmail.com>
2020-01-29 12:07:22 -0500

    Added AI
    1. Created an enemy prefab 2. Added dumb AI functionality

---
+++ W1/SoccerGame/Assets/Scripts/AIController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIController : MonoBehaviour
+{
+    //-- Public vars --//
+    public float speed; // Controls player speed
+    public GameObject ball; // Ball to follow
+
+    //-- Private vars --//
+    private Rigidbody2D rb; // 2D rigidbody to grab
+    private Vector2 direction;
+
+    void Start()
+    {
+        if (ball == null) Error.PrintError("Ball Gameobject not hooked up");
+
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        direction = (ball.transform.position - transform.position).normalized;
+        rb.velocity = new Vector2(direction.x * speed, direction.y * speed);
+    }
+}
