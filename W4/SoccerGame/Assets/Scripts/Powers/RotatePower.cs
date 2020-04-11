7b54a206e5b28e1744cf2c601c6b3eda812a23d5
zeeodd <evergreenskier@gmail.com>
2020-04-11 17:49:38 -0400

    Trail effect Added

--- W4/SoccerGame/Assets/Scripts/Powers/RotatePower.cs
+++ W4/SoccerGame/Assets/Scripts/Powers/RotatePower.cs
@@ -4,18 +4,22 @@ using UnityEngine;
 
 public class RotatePower : MonoBehaviour
 {
+    [Header("Rotate Power Attributes")]
     public KeyCode activationKey;
 
-    public float degreesPerSecond;
+    public float degreesPerFrame;
 
     public enum Direction { Left, Right }
     public Direction direction;
+
+    public TrailRenderer effect;
 
     /*
      * Private variables to control functionality
      */
     private bool gameHasStarted = false;
     private bool isRotating = false;
+    private bool effectAdded = false;
     private Quaternion restingRotation = new Quaternion(0f, 0f, 0f, 0f);
     private GameObject playerGameObject;
 
@@ -30,23 +34,51 @@ public class RotatePower : MonoBehaviour
         {
             playerGameObject = ServicesLocator.UserPlayer[0]._gameObject;
 
-            if (!isRotating) isRotating = true;
+            if (!isRotating)
+            {
+                Invoke("InitEffect", 0f);
+                isRotating = true;
+            }
             else 
             {
                 ResetRotation();
+                Invoke("DestroyEffect", 0f);
                 isRotating = false;
             } 
         }
 
-        if (isRotating)
-        {
-            playerGameObject.transform.Rotate(0f, 0f, degreesPerSecond);
-        }
+        if (isRotating) Rotate();
     }
 
     private void ResetRotation()
     {
         playerGameObject.transform.rotation = restingRotation;
+    }
+
+    private void Rotate()
+    {
+        if (direction.Equals(Direction.Left)) playerGameObject.transform.Rotate(0f, 0f, degreesPerFrame);
+
+        if (direction.Equals(Direction.Right)) playerGameObject.transform.Rotate(0f, 0f, -degreesPerFrame);
+    }
+
+    private void InitEffect()
+    {
+        if (!effectAdded)
+        {
+            GameObject trailEffect = Instantiate(Resources.Load<GameObject>("Prefabs/Trail"));
+            trailEffect.gameObject.transform.parent = playerGameObject.transform;
+            trailEffect.transform.localPosition = new Vector3(0f, 0f, 0f);
+        }
+        else
+        {
+            playerGameObject.transform.GetChild(0).GetComponent<TrailRenderer>().enabled = true;
+        }
+    }
+
+    private void DestroyEffect()
+    {
+        playerGameObject.transform.GetChild(0).GetComponent<TrailRenderer>().enabled = false;
     }
 
     private void InitializePowers(AGPEvent e)
